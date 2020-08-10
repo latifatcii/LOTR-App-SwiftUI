66e0d44703829dd9e895aa85415e4d0400ade4af
Abdullatif Atci <latif.800@gmail.com>
2020-08-10 13:17:21 +0300

    Initial Commit

---
+++ LOTR App SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LOTR App SwiftUI
+//
+//  Created by Latif Atci on 8/10/20.
+//  Copyright © 2020 Latif Atci. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
